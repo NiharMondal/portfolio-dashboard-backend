ec38381b7664bada4b555512a567efaa045d5c3f
NiharMondal <developernihar@gmail.com>
2024-07-13 12:45:14 +0600

    removed: origin

--- src/app.ts
+++ src/app.ts
@@ -7,22 +7,9 @@ import { rootRouter } from "./route";
 
 const app: Application = express();
 
-const allowedOrigins = [
-	"https://nihar-mondal.vercel.app/",
-	"https://portfolio-dashboard-client-iota.vercel.app/"
-];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // Check if the origin is in the allowed origins list
-    if (allowedOrigins.indexOf(origin as string) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials:true,
-}));
+
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
