d7a792eb1d837bed3f2268ceb4d9c8fba8708443
NiharMondal <developernihar@gmail.com>
2024-09-22 23:43:26 +0600

    added to: dev branch

--- src/modules/project/project.validation.ts
+++ src/modules/project/project.validation.ts
@@ -2,12 +2,11 @@ import z from 'zod'
 
 const createProject = z.object({
     title: z.string({required_error:'Title must required'}).trim(),
-    live_link: z.string({required_error:'Title must required'}),
-    front_end: z.string({required_error:'Title must required'}),
-    back_end: z.string({required_error:'Title must required'}).optional(),
-    description: z.string({required_error:'Title must required'}),
-    photo: z.string().optional(),
-
+    photo: z.string({message:"Photo url is required"}).url({message:"Must be a valid url"}),
+    liveLink: z.string({required_error:'Live link must required'}),
+    gitHubLink: z.string({required_error:'GitHub link must required'}),
+    description: z.string({required_error:'Description must required'}),
+    techtechnologies: z.string().array()
 
 })
 
